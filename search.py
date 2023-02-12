6e341232ab4fc57c241c3f232289026d8d9746b2
sibo-t <stmsabala@gmail.com>
2023-02-12 13:05:26 +0200

    getting date,usrname and tweet

--- search.py
+++ search.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from random import randint
 
 import time
 
 def run():
     driver = webdriver.Chrome()
     driver.get("https://www.twitter.com")
+    time.sleep(randint(3,7))
+    driver.find_element(By.TAG_NAME, "input").send_keys("#chelseabun" + Keys.ENTER)
     time.sleep(2)
-    driver.find_element(By.TAG_NAME, "input").send_keys("selenium" + Keys.ENTER)
-    time.sleep(20)
-    # driver.find_element(By.XPATH, "//*[@id=\"layers\"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div").click()
+    output = driver.find_elements(By.CLASS_NAME, "css-1dbjc4n")
+
+    for i in output:
+        print(i.text)
     driver.quit()
 
 if __name__ == "__main__" :
