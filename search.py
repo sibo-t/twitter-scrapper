58ae4eb30e8e6720ad8fb9832ff06680dd99bae7
sibo-t <stmsabala@gmail.com>
2023-02-14 14:05:50 +0200

    scroll down page

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
 from random import randint
 
 import time
@@ -10,12 +12,49 @@ def run():
     driver.get("https://www.twitter.com")
     time.sleep(randint(3,7))
     driver.find_element(By.TAG_NAME, "input").send_keys("#chelseabun" + Keys.ENTER)
-    time.sleep(2)
-    output = driver.find_elements(By.CLASS_NAME, "css-1dbjc4n")
 
-    for i in output:
-        print(i.text)
+    # output = retrieve_data(driver)
+
+    # for j in range(10):
+    #     output.extend(retrieve_data(driver))
+    #     ActionChains(driver)\
+    #         .send_keys(Keys.ESCAPE)\
+    #             .perform()
+    #     time.sleep(randint(3,7))
+        
+
+    count = 0
+    f= open("tweetdump2.txt","w+")
+
+    for i in range(10):
+        
+        f.write("*********************"+str(count)+"*************************\n")
+        f.write("\n")
+        f.write(retrieve_data(driver).text)
+        f.write("\n")
+        f.write("*******************************************************\n")
+        f.write("\n")
+        count+=1
+
+        ActionChains(driver)\
+            .send_keys(Keys.ESCAPE)\
+                .perform()
+        time.sleep(randint(3,7))
+
+    f.close()
+
     driver.quit()
+
+def retrieve_data(driver):
+    
+    output = driver.find_element(By.TAG_NAME, "main")
+    
+    ActionChains(driver)\
+        .scroll_by_amount(0, 600)\
+        .perform()
+
+    return output
+
 
 if __name__ == "__main__" :
     run()
